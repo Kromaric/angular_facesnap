fd37db35643da131f508666a2387011b8190fbb2
Kromaric <romaricyt11@gmail.com>
2025-07-10 01:22:26 +0200

    add single face snap

---
+++ src/app/single-face-snap/single-face-snap.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { FaceSnapModel } from "../models/face-snap";
+import { FaceSnapsService } from '../services/face-snaps.services';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-single-face-snap',
+  templateUrl: './single-face-snap.html',
+  styleUrls: ['./single-face-snap.scss']
+})
+export class SingleFaceSnap implements OnInit {
+  faceSnap!: FaceSnapModel;
+  buttonText!: string;
+
+  constructor(private faceSnapsService: FaceSnapsService,
+              private route: ActivatedRoute) {}
+
+  ngOnInit() {
+    this.buttonText = 'Oh Snap!';
+    const faceSnapId = +this.route.snapshot.params['id'];
+    //this.faceSnap = this.faceSnapsService.getFaceSnapById(faceSnapId);
+  }
+
+  onSnap() {
+    if (this.buttonText === 'Oh Snap!') {
+      this.faceSnapsService.snapFaceSnapById(this.faceSnap.id, 'snap');
+      this.buttonText = 'Oops, unSnap!';
+    } else {
+      this.faceSnapsService.snapFaceSnapById(this.faceSnap.id, 'unsnap');
+      this.buttonText = 'Oh Snap!';
+    }
+  }
+}
