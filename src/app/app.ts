06eaf7204a9a49f7809a019c8c5c3588dac93b64
Kromaric <romaricyt11@gmail.com>
2025-07-15 23:05:47 +0200

    mise à jour

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,18 +1,42 @@
 import { Component } from '@angular/core';
 import {Header} from "./header/header";
 import { RouterOutlet } from '@angular/router';
+import { interval, Observable, map, filter, tap, of} from 'rxjs';
+import  { AsyncPipe } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
 
 
 @Component({
   selector: 'app-root',
-  
+
   templateUrl: './app.html',
   styleUrl: './app.scss',
   imports: [
     RouterOutlet,
     Header,
+    AsyncPipe
+
   ]
 })
 export class App{
-  
+  interval$!: Observable<string>;
+
+  ngOnInit() {
+
+    this.interval$ = interval(1000).pipe(
+      filter(value => value % 3 === 0),
+      map((value: number) => value % 2 === 0 ?
+       `je suis ${value} et je suis pair` :
+       `je suis ${value} et je suis impair`
+      ),
+      tap(text => this.logger(text))
+    );
+
+
+
+  }
+  logger(text:string) {
+    console.log(`Log: ${text}`);
+  }
 }
--- src/app/face-snap-list/face-snap-list.ts
+++ src/app/face-snap-list/face-snap-list.ts
@@ -1,22 +1,36 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FaceSnap } from "../face-snap/face-snap";
-import {FaceSnapModel} from "../models/face-snap";
+import { FaceSnapModel } from "../models/face-snap";
 import { FaceSnapsService } from '../services/face-snaps.services';
+import { Observable, interval, Subject } from 'rxjs';
+import { takeUntil, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-face-snap-list',
+  standalone: true,
   imports: [FaceSnap],
   templateUrl: './face-snap-list.html',
-  styleUrl: './face-snap-list.scss'
+  styleUrls: ['./face-snap-list.scss']
 })
-export class FaceSnapList implements OnInit {
- faceSnaps!: FaceSnapModel[];
+export class FaceSnapList implements OnInit, OnDestroy {
+  faceSnaps!: FaceSnapModel[];
+  private destroy$!: Subject<boolean>;
 
- constructor(private faceSnapsService: FaceSnapsService) {}
+  constructor(private faceSnapsService: FaceSnapsService) {}
 
   ngOnInit() {
+    this.destroy$ = new Subject<boolean>();
     this.faceSnaps = this.faceSnapsService.getFaceSanps();
+
+    interval(1000).pipe(
+      takeUntil(this.destroy$),
+      tap(console.log)
+    ).subscribe();
   }
 
-
+  ngOnDestroy(): void {
+    this.destroy$.next(true);
+  }
 }
+
+
